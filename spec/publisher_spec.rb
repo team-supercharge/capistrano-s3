62cf1f7e9cd4dc398388afdd1a90796498eaa309
Brian Knight <brianknight10@gmail.com>
2014-06-05 11:17:00 -0400

    Fix broken spec due to AWS SDK change.

--- spec/publisher_spec.rb
+++ spec/publisher_spec.rb
@@ -9,7 +9,7 @@ describe Capistrano::S3::Publisher do
 
   context "on publish!" do
     it "publish all files" do
-      AWS::S3::Client.any_instance.expects(:put_object).times(4)
+      AWS::S3::Client::V20060301.any_instance.expects(:put_object).times(4)
 
       path = File.join(@root, 'sample')
       Capistrano::S3::Publisher.publish!('s3.amazonaws.com', 'abc', '123', 'mybucket.amazonaws.com', path, {})
